35a6cd3c36e0de8cfc049aa7a69bdb4320f071ce
Chiñas <55107045+Angelch09@users.noreply.github.com>
2022-08-18 23:19:27 -0500

    Comentario sobre código de ejemplo

--- src/com/acme/PersonaRegistro.java
+++ src/com/acme/PersonaRegistro.java
@@ -3,6 +3,8 @@ package com.acme;
 public record PersonaRegistro(String nombre, String apellidos) {
     @Override
     public String toString() {
+        //Angel
+        //Comenté esta linea porque tal vez se puede eliminar
         System.out.println("Instrucción de ejemplo");
         return "%s %s".formatted(nombre, apellidos);
     }
