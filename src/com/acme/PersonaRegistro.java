66e6ae8549841cbb93b75fff63f4ecfa7318017f
chiñas <angelch98@hotmail.com>
2022-08-18 23:17:28 -0500

    Se eliminó el comentario de ejemplo

--- src/com/acme/PersonaRegistro.java
+++ src/com/acme/PersonaRegistro.java
@@ -3,9 +3,6 @@ package com.acme;
 public record PersonaRegistro(String nombre, String apellidos) {
     @Override
     public String toString() {
-        //Angel
-        //Comenté esta linea porque tal vez se puede eliminar
-        System.out.println("Instrucción de ejemplo");
         return "%s %s".formatted(nombre, apellidos);
     }
 }
