d82dd9b0b65a1a62deb72685c9ae775588737a6d
chiñas <angelch98@hotmail.com>
2022-08-18 22:51:09 -0500

    Se agregó una linea de codigo de ejemplo

--- src/com/acme/PersonaRegistro.java
+++ src/com/acme/PersonaRegistro.java
@@ -3,6 +3,7 @@ package com.acme;
 public record PersonaRegistro(String nombre, String apellidos) {
     @Override
     public String toString() {
+        System.out.println("Instrucción de ejemplo");
         return "%s %s".formatted(nombre, apellidos);
     }
 }
